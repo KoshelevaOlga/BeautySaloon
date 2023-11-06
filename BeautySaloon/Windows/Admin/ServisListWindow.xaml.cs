0e62219c6ca38591655abb6b302bf74af26ef89d
KoshelevaOlga <126570872+KoshelevaOlga@users.noreply.github.com>
2023-11-06 20:58:28 +0300

    Add code (#10)
    * Add Code to OrderListUpdate
    
    * Add Code
    
    * Добавление окон у админа

--- BeautySaloon/Windows/Admin/ServisListWindow.xaml.cs
+++ BeautySaloon/Windows/Admin/ServisListWindow.xaml.cs
@@ -22,6 +22,54 @@ namespace BeautySaloon.Windows.Admin
         public ServisListWindow()
         {
             InitializeComponent();
+            GetServiceList();
+        }
+        private void GetServiceList()
+        {
+            LvService.ItemsSource = ClassHelper.EF.Context.vw_ServicList.ToList();
+        }
+        private void BtnExit_Clic(object sender, RoutedEventArgs e)
+        {
+            AuthWindow authWindow = new AuthWindow();
+            authWindow.Show();
+            this.Close();
+        }
+
+        private void BtnOrder_Clic(object sender, RoutedEventArgs e)
+        {
+            Admin.OrderListWindow orderListWindow = new Admin.OrderListWindow();
+            orderListWindow.Show();
+            this.Close();
+        }
+
+        private void BtnEmployer_Clic(object sender, RoutedEventArgs e)
+        {
+            Admin.StafListWindow stafListWindow = new Admin.StafListWindow();
+            stafListWindow.Show();
+            this.Close();
+        }
+
+        private void BtnNewService_Clic(object sender, RoutedEventArgs e)
+        {
+            Admin.NewServisWindow newServisWindow = new Admin.NewServisWindow();
+            newServisWindow.Show();
+        }
+
+        private void BtnDelit_Clic(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnApdate_Clic(object sender, RoutedEventArgs e)
+        {
+            if (LvService.SelectedItem == null)
+            {
+                return;
+            }
+            var service = LvService.SelectedItem as BD.Service;
+            ClassHelper.Services.ServiceCard.Add(service);
+            Admin.EditServiceWindow editServiceWindow = new Admin.EditServiceWindow(service);
+            editServiceWindow.Show();
         }
     }
 }
